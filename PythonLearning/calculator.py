e2c5b7a15394dcc0526e8caf204395f9b44fc647
dhanaamalakanta <dhana.amalakanta@gmail.com>
2020-03-31 21:28:49 +0530

    commit on calculations

---
+++ PythonLearning/calculator.py
@@ -0,0 +1,28 @@
+
+# sum
+#sub
+#mult
+#divide
+
+def sum(x,y):
+    print ("received first argument" + str(x))
+    print ("Second argument is " + str(y))
+    z=x+y
+    return z
+
+def sub(x,y):
+    print ("received first argument" + str(x))
+    print ("Second argument is " + str(y))
+    z=x-y
+    return z
+def mul(x,y):
+    print ("received first argument" + str(x))
+    print ("Second argument is " + str(y))
+    z=x*y
+    return z
+
+def div(x,y):
+    print ("received first argument" + str(x))
+    print ("Second argument is " + str(y))
+    z = x / y
+    return z
