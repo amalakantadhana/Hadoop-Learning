cdf9b7e96bede198793bf9353bf4e4e13f05daa7
dhanaamalakanta <dhana.amalakanta@gmail.com>
2020-03-31 13:32:00 +0530

    second commit on variables names

--- PythonLearning/Variable.py
+++ PythonLearning/Variable.py
@@ -3,3 +3,14 @@ age=25
 name='dhanna'
 print age
 print name
+
+# second one
+x=4 # x is type of int
+x="sallyy" # x is type of str
+print(x)
+
+# third
+x = "John"
+# is the same as
+x = 'John'
+print x
