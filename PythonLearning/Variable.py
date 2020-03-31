6d82cbbc22991a77834d65d87d18ee3dc5373d21
dhanaamalakanta <dhana.amalakanta@gmail.com>
2020-03-31 21:28:07 +0530

    commit on email and add

--- PythonLearning/Variable.py
+++ PythonLearning/Variable.py
@@ -1,3 +1,4 @@
+from PythonLearning import calculator, Email
 
 age=25
 name='dhanna'
@@ -14,3 +15,30 @@ x = "John"
 # is the same as
 x = 'John'
 print x
+
+output=calculator.sum(10,30)
+print ("the final output is " + str(output))
+
+output=calculator.sum(50,40)
+print ("the final output is " + str(output))
+
+output=calculator.sub(40,30)
+print ("the final output is " + str(output))
+
+output=calculator.mul(40,30)
+print ("the final output is " + str(output))
+
+output=calculator.div(40,30)
+print ("the final output is " + str(output))
+
+output=Email.emailgmail("dhana","lakshmi")
+print ("the final output is " + str(output))
+
+output=Email.emailgmail("nagi","avr")
+print ("the final output is " + str(output))
+
+output=Email.emailyahoo("dhana","lakshmi")
+print ("the final output is " + str(output))
+
+output=Email.emailyahoo("nagi","avr")
+print ("the final output is " + str(output))
