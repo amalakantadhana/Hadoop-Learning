3b07378bd876d2d238541db16852a1b637f8fe69
dhanaamalakanta <dhana.amalakanta@gmail.com>
2020-04-28 12:55:37 +0530

    commit on if string

--- PythonLearning/equal_check.py
+++ PythonLearning/equal_check.py
@@ -14,31 +14,45 @@ equalcheck(9,9)
 #3. Write a function to take 1 input variable and check whether variable is equal to "hyderabad". Case insensitive.
 
 def stringsensitive(string1):
-    if string1== string1.lower():
-        print(string1.lower() + "The strings are the same (case insensitive)")
-    else:
-        print("The strings are NOT the same (case insensitive)")
-
-stringsensitive("apple")
-
-#4. Write a function to take 1 input variable and check whether variable is equal to "hyderabad". Case sensitive.
-
-def stringsensitive(string1):
-    if string1!= string1.lower():
+    if string1=="hyderabad":
         print("The strings are the same (case insensitive)")
     else:
         print("The strings are NOT the same (case insensitive)")
 
-stringsensitive("APPLE")
+stringsensitive("HYDERABAD")
+stringsensitive("hyderabad")
+stringsensitive("apple")
+#4. Write a function to take 1 input variable and check whether variable is equal to "hyderabad". Case sensitive.
+
+def stringsensitive(string1):
+    string2=string1.lower();
+    if string2=="hyderabad":
+        print("The strings are the same (case sensitive)")
+    else:
+        print("The strings are NOT the same (case sensitive)")
+
+stringsensitive("apple")
 #22. Write a function to take 2 input variables and check whether first string is substring of second one.
 #Example : nag,nagendra - True
 #Example : abc,nagendra - False
 def matchstring(a,b):
-        if (a.find(b) != -1):
+        if (a.find(b) != -1): #find returns integer.
             print ("true")
         else:
-            print(a.find(b))
+            print("false")
 
 matchstring("nag", "nagendra")
+
+# input two variables
+
+def inputvar(string1,string2):
+    string3=string1.lower();
+    string4=string2.lower();
+    if(string3==string4):
+        print("string are eual")
+    else:
+        print("string are not equal")
+
+inputvar("apple", "mango")
 
 
