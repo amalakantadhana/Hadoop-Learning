98b2eea6610a35752f498fef9d8a0261cbafc3f4
dhana.amalakanta@gmail.com <dhana.amalakanta@gmail.com>
2022-01-10 01:23:31 -0800

    by comamn prompt

--- PythonLearning/calculator.py
+++ PythonLearning/calculator.py
@@ -16,7 +16,9 @@ def sum(x,y):
 def sub(x,y):
     print ("received first argument" + str(x))
     print ("Second argument is " + str(y))
-    print("this is a develop")
+    print("this is a develop"
+    print("this is a develop1")
+
     z=x-y
     return z
 def mul(x,y):
