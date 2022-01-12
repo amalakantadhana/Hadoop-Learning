74af06c5c854c6547f2f5d823144ac912f7a00a0
dhana.amalakanta@gmail.com <dhana.amalakanta@gmail.com>
2022-01-12 00:44:37 -0800

    This is python practise

---
+++ firstprogram.py
@@ -0,0 +1,1 @@
+print("helow welcome python programmingg")
---
+++ variableexample.py
@@ -0,0 +1,29 @@
+firstname="Dhana"
+rollno=20
+height=5.9
+print(type(firstname))
+print(type(rollno))
+print(type(height))
+
+nam1="dhana"
+nam2="lakshmi"
+print(nam1, end="")
+print(nam2)
+
+mystr= "This is a string variable"
+print(mystr[0:5])
+
+fruit=['apple','banana','orange','strawberry']
+print(fruit)
+fruit.append("berry")
+print(fruit)
+fruit.insert(1, "pineapple")
+print(fruit)
+fruit.sort()
+print(fruit)
+fruit.reverse()
+print(fruit)
+fruit.remove(1)
+print(fruit)
+fruit.insert(0, "apple")
+print(fruit)
